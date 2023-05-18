af24aac1ad6e660babcd54531aa0e84577d32666
K-Takanori <tabo.25.apr.1984@gmail.com>
2023-05-18 16:03:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Avatar, Col, Input, Row } from 'antd';
+import { Typography, Layout, Button } from 'antd';
+import axios from "axios";
+const { Title } = Typography;
+const { Header, Content, Footer } = Layout;
 
 function App() {
+  const [chats, setChats] = useState([])
+  const [res, setRes] = useState([])
+  const [text, setText] = useState("")
+  const [alldata, setAlldata] = useState([])
+
+  const changeText = (e) => {
+    setText(e.target.value)
+  }
+
+  const apiRequest = () =>{
+    axios
+    .get(process.env.REACT_APP_API_SERVER+text)             //リクエストを飛ばすpath
+    .then(response => {
+      const newres = [...res]
+      newres.push(response.data["response"])
+      setRes(newres)  
+    })                               //成功した場合、postsを更新する（then）
+    .catch(() => {
+        console.log('通信に失敗しました');
+    });
+  }
+
+  const inputData = () => {
+    const newChats = [...chats]
+    newChats.push(text)
+    setChats(newChats)
+    apiRequest()
+  }
+
+  useEffect(() => {
+    const len = chats.length + res.length
+    const tmpdata = []
+    for (let i = 0; i < Math.round(len / 2); i++){
+      tmpdata.push(chats[i])
+      if(!(i * 2 > len)){
+        tmpdata.push(res[i])
+      }
+    }
+    setAlldata(tmpdata)
+  },[chats,res]);
+
+  console.log(process.env.REACT_APP_API_SERVER)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Layout className="App">
+    <Header>
+      <Title style={{color:"white"}}>Chat Bot</Title>
+    </Header>
+    <Content style={{padding: 24,minHeight: 800,}}>
+      <Row>
+        <Col flex="auto">
+          <Input placeholder='聞きたいことを入力してください' onChange={(e) => changeText(e)} />
+        </Col>
+        <Col flex="none">
+          <Button type="primary" onClick={inputData}>送信</Button>
+        </Col>      
+      </Row>
+      {alldata.map((value, i) => i % 2 == 0 ? <Row key={i}><Col span={10} offset={14} style={{overflowWrap:"breakWord"}}>{value}</Col></Row> : <Row key={i}><Col span={10} style={{overflowWrap:"breakWord"}}>{value}</Col></Row>)}
+    </Content>
+  </Layout>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux"
+import "./App.css";
+import store from './store/index'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Redux Learn</h1>
+//       <p>Count:{store.getState().count}</p>
+//     </div>
+//   );
+// }
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
